9af8d2dfe6ee5fb2085f87b92543d8f673946a54
PiyushJ1 <piyxsh@gmail.com>
2026-01-14 00:27:46 +1100

    feat: init app

---
+++ src/poller/main.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+# init app
+app = FastAPI(
+    title="Spotify Poller",
+    description="Service to poll the spotify api and push events to queue"
+)
+
+# define a basic route
+@app.get("/")
+def read_root():
+    return {
+        "status": "online",
+        "service": "poller"
+    }
+
