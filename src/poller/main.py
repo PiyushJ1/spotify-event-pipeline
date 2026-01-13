b169ba23efaadf533d84b9b1458ba825742518d0
PiyushJ1 <piyxsh@gmail.com>
2026-01-14 00:30:39 +1100

    style: black

--- src/poller/main.py
+++ src/poller/main.py
@@ -5,14 +5,11 @@ from fastapi import FastAPI
 # init app
 app = FastAPI(
     title="Spotify Poller",
-    description="Service to poll the spotify api and push events to queue"
+    description="Service to poll the spotify api and push events to queue",
 )
+
 
 # define a basic route
 @app.get("/")
 def read_root():
-    return {
-        "status": "online",
-        "service": "poller"
-    }
-
+    return {"status": "online", "service": "poller"}
