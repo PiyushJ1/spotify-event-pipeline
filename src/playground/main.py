91d314dc213bcf9ecb776efc25d5a83984ed1c3b
PiyushJ1 <piyxsh@gmail.com>
2026-01-27 03:12:34 +1100

    feat: request auth token from spotify

---
+++ src/playground/main.py
@@ -0,0 +1,31 @@
+import boto3
+import requests
+import os
+from dotenv import load_dotenv
+import requests
+import base64
+
+load_dotenv()
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+QUEUE_NAME = "test-queue"
+AWS_ENDPOINT = "http://localhost:4566" # points to localstack
+spotify_api_url = "https://accounts.spotify.com/api/token"
+
+def get_auth_token():
+    encoded = base64.b64encode((client_id + ":" + client_secret).encode("utf-8")).decode("utf-8")
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + encoded
+    }
+
+    payload = {
+        "grant_type": "client_credentials",
+    }
+
+    response = requests.post(spotify_api_url, headers=headers, data=payload)
+
+    print(response.text)
+
+get_auth_token()
