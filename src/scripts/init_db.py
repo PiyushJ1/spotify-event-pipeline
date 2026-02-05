86a37a5ee080598311d83204abd79191159d4405
PiyushJ1 <piyxsh@gmail.com>
2026-02-05 22:32:57 +1100

    chore: import new tables to init within db

--- src/scripts/init_db.py
+++ src/scripts/init_db.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.getcwd())
 
 from src.common.db import engine, Base
-from src.common.models import SimpleSong
+from src.common.models import SimpleSong, AuthToken, Track, ListeningHistory
 
 
 def init_db():
