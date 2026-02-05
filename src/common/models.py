e05832371235a0c4e902cd56eb5ce11a3d28cf3f
PiyushJ1 <piyxsh@gmail.com>
2026-02-05 22:05:30 +1100

    feat: create table for auth token + song track

--- src/common/models.py
+++ src/common/models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from src.common.db import Base
 
 
+# simple song table to play around with
 class SimpleSong(Base):
     __tablename__ = "simple_songs"
 
@@ -11,3 +12,32 @@ class SimpleSong(Base):
 
     # prevent duplicates (idempotency)
     spotify_id = Column(String, unique=True)
+
+
+# spotify OAuth2 token
+class AuthToken(Base):
+    __tablename__ == "auth_tokens"
+
+    id = Column(Integer, primary_key=True)
+    access_token = Column(String, nullable=False)
+    refresh_token = Column(String, nullable=False)
+    expires_at = Column(Integer, nullable=False)
+    updated_at = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+
+# proper full details for a track
+class Track(Base):
+    __tablename__ == "tracks"
+
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
+    artist = Column(String, nullable=False)
+    album = Column(String)
+
+    image_url = Column(String)
+    duration_ms = Column(String)
+    popularity = Column(Integer)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
