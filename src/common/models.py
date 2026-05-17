9bb1cf85b62a487f07c1e97c96260132aa9c1e97
PiyushJ1 <piyxsh@gmail.com>
2026-05-17 17:41:28 +1000

    feat: introduce type hinting + other conventions in accordance to sqlalchemy 2.0

--- src/common/models.py
+++ src/common/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, UniqueConstraint
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from datetime import datetime
 from sqlalchemy.sql import func
 from src.common.db import Base
 
@@ -8,74 +9,84 @@ from src.common.db import Base
 class SimpleSong(Base):
     __tablename__ = "simple_songs"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    artist = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    artist: Mapped[str]
 
     # prevent duplicates (idempotency)
-    spotify_id = Column(String, unique=True)
+    spotify_id: Mapped[str] = mapped_column(unique=True)
 
 
 # user info table
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    spotify_user_id = Column(String, unique=True, nullable=False, index=True)
-    display_name = Column(String, nullable=True)
-    created_at = Column(DateTime, server_default=func.now())
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    spotify_user_id: Mapped[str] = mapped_column(unique=True, index=True)
+    display_name: Mapped[str | None]
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
-    auth_token = relationship("AuthToken", back_populates="user", uselist=False)
+    auth_token: Mapped["AuthToken"] = relationship(back_populates="user", uselist=False)
 
 
 # spotify OAuth2 token
 class AuthToken(Base):
     __tablename__ = "auth_tokens"
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
-    access_token = Column(String, nullable=False)
-    refresh_token = Column(String, nullable=False)
-    expires_at = Column(Integer, nullable=False)
-    updated_at = Column(
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+    access_token: Mapped[str]
+    refresh_token: Mapped[str]
+    expires_at: Mapped[int]
+    updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
 
-    user = relationship("User", back_populates="auth_token")
+    user: Mapped["User"] = relationship(back_populates="auth_token")
 
 
 # proper full details for a track
 class Track(Base):
     __tablename__ = "tracks"
 
-    id = Column(String, primary_key=True)
-    name = Column(String, nullable=False)
-    artist = Column(String, nullable=False)
-    album = Column(String)
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    artist: Mapped[str]
+    album: Mapped[str]
+    image_url: Mapped[str]
+    duration_ms: Mapped[int]
+    popularity: Mapped[int]
 
-    image_url = Column(String)
-    duration_ms = Column(Integer)
-    popularity = Column(Integer)
-
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
 
 
 # stores ALL songs played by user
 class ListeningHistory(Base):
     __tablename__ = "listening_history"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    track_id = Column(String, ForeignKey("tracks.id"), nullable=False)
-    track_name = Column(String, nullable=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    played_at = Column(DateTime(timezone=True), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    track_id: Mapped[str] = mapped_column(ForeignKey("tracks.id"))
+    track_name: Mapped[str | None]
+    artist: Mapped[str | None]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    played_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
-    ingested_at = Column(DateTime(timezone=True), server_default=func.now())
+    ingested_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
 
-    track = relationship("Track")  # link the tables
-    user = relationship("User")
+    # link the tables
+    track: Mapped["Track"] = relationship()
+    user: Mapped["User"] = relationship()
 
     # idempotency (prevent duplicate track events from being stored)
     __table_args__ = (
-        UniqueConstraint("track_id", "played_at", name="uix_track_played_at"),
+        UniqueConstraint(
+            "user_id",
+            "track_id",
+            "played_at",
+            name="uix_track_played_at",  # uix = unique index/constraint
+        ),
     )
