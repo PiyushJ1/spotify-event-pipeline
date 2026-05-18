53642951ea915f4bacc8787b9963d96b7360d198
PiyushJ1 <piyxsh@gmail.com>
2026-05-19 00:18:56 +1000

    chore: update polling handler for prod sqs

--- scripts/run_poller_handler.py
+++ scripts/run_poller_handler.py
@@ -18,19 +18,9 @@ ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if ROOT not in sys.path:
     sys.path.insert(0, ROOT)
 
-load_dotenv()
-
-# Local defaults for running against LocalStack during development
-os.environ.setdefault("AWS_ACCESS_KEY_ID", os.environ.get("AWS_ACCESS_KEY_ID", "test"))
-os.environ.setdefault(
-    "AWS_SECRET_ACCESS_KEY", os.environ.get("AWS_SECRET_ACCESS_KEY", "test")
-)
-os.environ.setdefault(
-    "SQS_ENDPOINT_URL", os.environ.get("SQS_ENDPOINT_URL", "http://localhost:4566")
-)
+load_dotenv(override=True)
 
 from src.poller.handler import handler
-
 
 if __name__ == "__main__":
     while True:
