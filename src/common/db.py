c7b7006a54e158e6276ddf4b8f1c7c353ac8bc3f
PiyushJ1 <piyxsh@gmail.com>
2026-05-19 00:23:24 +1000

    fix: override env variable + fix type hint

--- src/common/db.py
+++ src/common/db.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = str(os.getenv("DATABASE_URL"))
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
